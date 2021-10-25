fd096b9062988b861ee56d8923e8eee1bea7b9c9
aditya-bhawsar-mm <89836238+aditya-bhawsar-mm@users.noreply.github.com>
2021-10-25 13:58:38 +0530

    Update decrypt_secrets.sh

--- .github/scripts/decrypt_secrets.sh
+++ .github/scripts/decrypt_secrets.sh
@@ -3,5 +3,5 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" / 
---output $HOME/secrets/my_secret.json google-services.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output $HOME/secrets/my_secret.json my_secret.json.gpg
